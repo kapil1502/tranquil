e5939448284b499191f8d83b5a72c186408ad5c8
Kapil Verma <kapil.verma.dev1@gmail.com>
2025-02-02 22:38:50 +0530

    Update eslint.config.mjs

--- Frontend/eslint.config.mjs
+++ Frontend/eslint.config.mjs
@@ -14,6 +14,8 @@ const eslintConfig = [
     extends: ["next/core-web-vitals", "next/typescript"],
     rules: {
       'react/no-unescaped-entities': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off'
     },
   }),
 ]
